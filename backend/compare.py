5b768bc55680be16cb79ad752f90f242e4b04e62
Om Ghag <58267725+OmGhag@users.noreply.github.com>
2024-09-24 16:06:05 +0530

    Complete

--- backend/compare.py
+++ backend/compare.py
@@ -1,22 +1,27 @@
 
 from PIL import Image
 import numpy as np
+import cv2
+from keras.models import load_model
+import os
 
 def compare_images(image1_path, image2_path):
     """Compare two images and return similarity score."""
-    img1 = Image.open(image1_path)
-    img2 = Image.open(image2_path)
-
-    # Resize images to the same size
-    img1 = img1.resize((256, 256))
-    img2 = img2.resize((256, 256))
-
-    # Convert images to numpy arrays
-    arr1 = np.array(img1)
-    arr2 = np.array(img2)
+    
+    imgs = np.empty((2, 96, 96), dtype = np.uint8)
+    img = cv2.imread(image1_path, cv2.IMREAD_GRAYSCALE)
+    img = cv2.resize(img, (96, 96)).astype(np.float32) / 255.
+    imgs[0] = img
+    
+    img = cv2.imread(image2_path, cv2.IMREAD_GRAYSCALE)
+    img = cv2.resize(img, (96, 96)).astype(np.float32) / 255.
+    imgs[1] = img
+    
+    rx1 = imgs[0].reshape((1, 96, 96)).astype(np.float32) / 255.
+    rx2 = imgs[1].reshape((1, 96, 96)).astype(np.float32) / 255.
 
     # Calculate the difference
-    diff = np.sum(np.abs(arr1 - arr2))
-    similarity = 100 - (diff / (256 * 256 * 3)) * 100  # Simple similarity score
+    model = load_model(r"C:\Users\omgha\OneDrive\Documents\GitHub\Streamlit-Image-Matching-\backend\FpM_Model_1.h5")
+    similarity  = model.predict([rx1, rx2])
 
     return similarity
